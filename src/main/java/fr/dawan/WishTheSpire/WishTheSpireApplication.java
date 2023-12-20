898f5261a0c6a117db0e0192b7e64cafecb10a4b
FlorianDray <floriandray97@gmail.com>
2023-12-20 10:58:07 +0100

    Mise en place Gitignore

---
+++ src/main/java/fr/dawan/WishTheSpire/WishTheSpireApplication.java
@@ -0,0 +1,13 @@
+package fr.dawan.WishTheSpire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WishTheSpireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WishTheSpireApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/dawan/WishTheSpire/WishTheSpireApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.dawan.WishTheSpire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WishTheSpireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
