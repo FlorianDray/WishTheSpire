c63049230dac46a258d2c07e5cea82eb56c33a3c
FlorianDray <floriandray97@gmail.com>
2023-12-22 12:03:43 +0100

    Add test Profile

--- src/test/java/fr/dawan/wish_the_spire/WishTheSpireApplicationTests.java
+++ src/test/java/fr/dawan/wish_the_spire/WishTheSpireApplicationTests.java
@@ -2,8 +2,10 @@ package fr.dawan.wish_the_spire;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class WishTheSpireApplicationTests {
 
 	@Test
